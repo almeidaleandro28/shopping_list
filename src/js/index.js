c0900a4c8dc7b0fb961a896ee1fa16d8fd6c88ed
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-02-22 10:00:04 -0300

    add archive js

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".box");
+const product = document.querySelector(".input");
+const btAdd = document.querySelector(".btn");
+
+console.log( product)
+console.log( btAdd)
+
+btAdd.addEventListener("click", () =>{
+
+} );
