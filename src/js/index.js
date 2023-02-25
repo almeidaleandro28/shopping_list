08730eadad9cbf42c9cb2c29bf8402d80d33ccd7
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-02-25 09:08:37 -0300

    btn bought create

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 const btAdd = document.querySelector(".btn");
 
+const createBtnBought = ( ) => {
+	const btn = document.createElement("button");
+	btn.innerHTML = "brought";
+	btn.classList.add("btn")
+	return btn;
+}
+
+const createBtnFinished =  () => {
+
+}
+
 
 const createProduct = ( ) => {
 	const product = document.querySelector(".input").value;
@@ -9,13 +20,16 @@ const createProduct = ( ) => {
 	const li = document.createElement("li");
 
 	li.innerHTML = `<p>${product}</p>`;
+	li.classList.add("box_item");
+	li.append( createBtnBought());
 
+	console.log( createBtnBought());
 	return list.appendChild( li );
- 
 }
+
+
 
 btAdd.addEventListener("click", ( e ) =>{
   e.preventDefault();
   createProduct()
-  
 } );
